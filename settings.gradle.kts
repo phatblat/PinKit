94005206a39fa6efc069afa749b8b2bae3931095
Ben Chatelain <ben@octop.ad>
2019-02-18 15:32:21 -0700

    🐘 Configure composite gradle project to embed SwiftPM plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,27 @@
  */
 
 rootProject.name = "PinKit"
+
+val swiftPM = file("../../gradle/SwiftPM-Plugin")
+if (swiftPM.exists()) {
+    includeBuild(swiftPM) {
+        dependencySubstitution {
+            substitute(module("at.phatbl:swiftpm")).with(project(":"))
+        }
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "at.phatbl.swiftpm" ->
+                    useModule("at.phatbl:swiftpm:${requested.version}")
+                else -> Unit
+            }
+        }
+    }
+}
