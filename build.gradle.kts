1e8fae98c47447dccd3afb62fa6a7fa05c0bfb81
Ben Chatelain <ben@octop.ad>
2019-02-19 21:54:20 -0700

    🐘👽 Create gradle pipeline tasks
    Used new configuration avoidance API (added in 4.9)
    https://docs.gradle.org/current/userguide/task_configuration_avoidance.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,61 @@
  * PinKit
  */
 
+/* -------------------------------------------------------------------------- */
+// 🛃 Imports
+/* -------------------------------------------------------------------------- */
+
+import org.gradle.api.DefaultTask
+
+/* -------------------------------------------------------------------------- */
+// 🔌 Plugins
+/* -------------------------------------------------------------------------- */
+
 plugins {
-  id("at.phatbl.swiftpm") version "1.1.0"
+    base
+    // id("at.phatbl.clamp") version "1.0.0"
+    id("at.phatbl.swiftpm") version "1.1.0"
+}
+
+/* -------------------------------------------------------------------------- */
+// ☑️ Tasks
+/* -------------------------------------------------------------------------- */
+
+open class PinKitTask : DefaultTask() {
+    init {
+        group = "📌 PinKit"
+    }
+}
+
+tasks {
+    named("clean").configure {
+        dependsOn("swiftpmClean")
+        dependsOn("swiftpmReset")
+    }
+
+    named("assemble").configure {
+        dependsOn("swiftBuild")
+    }
+
+    register<PinKitTask>("test") {
+        dependsOn("swiftTest")
+    }
+
+    register<PinKitTask>("codeQuality") {
+        doLast { logger.lifecycle("$name task not implemented") }
+    }
+
+    named("build").configure {
+        dependsOn("assemble")
+        dependsOn("test")
+        dependsOn("codeQuality")
+    }
+
+    register<PinKitTask>("release") {
+        doLast { logger.lifecycle("$name task not implemented") }
+    }
+
+    register<PinKitTask>("deploy") {
+        doLast { logger.lifecycle("$name task not implemented") }
+    }
 }
