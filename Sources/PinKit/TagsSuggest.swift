51d37a43fc2103dca29ae5ce23aeba5523f5ce91
Ben Chatelain <ben@octop.ad>
2020-08-30 16:24:17 -0600

    💡 Document TagsSuggest

--- Sources/PinKit/TagsSuggest.swift
+++ Sources/PinKit/TagsSuggest.swift
@@ -5,6 +5,9 @@
 //  Created by Ben Chatelain on 8/30/20.
 //
 
+/// List of tag suggestions for a given URL. Renamed from posts/suggest as listed in the Pinboard API since
+///  this endpoint returns tags, not pages.
+/// https://pinboard.in/api/#posts_suggest
 struct TagsSuggest: Decodable {
     let popular: [String]
     let recommended: [String]
